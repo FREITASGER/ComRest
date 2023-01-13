c60255a877d151fdac853706c9d1c1bb604f9c2c
Germán Rodríguez Serrano <grs@fhecor.es>
2023-01-13 19:58:15 +0100

    Add resstaurante

---
+++ app/src/main/java/com/example/comrest/RestarurantRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.comrest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestarurantRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restarurant_register);
+    }
+}
